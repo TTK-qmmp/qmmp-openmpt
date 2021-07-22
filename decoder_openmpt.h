30e42b89776a7f20aa94b3769eba373814354a69
greedysky <greedysky@163.com>
2021-07-22 16:00:06 +0800

    refactor: Optimized input module

---
+++ decoder_openmpt.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *   Copyright (C) 2013-2021 by Ilya Kotov                                 *
+ *   forkotov02@ya.ru                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef DECODER_OPENMPT_H
+#define DECODER_OPENMPT_H
+
+#include "openmpthelper.h"
+#include <qmmp/decoder.h>
+
+/**
+    @author Ilya Kotov <forkotov02@ya.ru>
+*/
+class DecoderOpenMPT : public Decoder
+{
+public:
+    DecoderOpenMPT(QIODevice *input);
+    virtual ~DecoderOpenMPT();
+
+    // Standard Decoder API
+    virtual bool initialize() override;
+    virtual qint64 totalTime() const override;
+    virtual int bitrate() const override;
+    virtual qint64 read(unsigned char *data, qint64 maxSize) override;
+    virtual void seek(qint64 time) override;
+
+private:
+    OpenMPTHelper *m_helper = nullptr;
+
+};
+
+#endif
---
+++ openmpt.pro
@@ -0,0 +1,28 @@
+include($$PWD/../../plugins.pri)
+
+TARGET = $$PLUGINS_PREFIX/Input/openmpt
+
+HEADERS += decoderopenmptfactory.h \
+           decoder_openmpt.h \
+           openmptmetadatamodel.h \
+           openmpthelper.h \
+           settingsdialog.h
+
+SOURCES += decoderopenmptfactory.cpp \
+           decoder_openmpt.cpp \
+           openmptmetadatamodel.cpp \
+           openmpthelper.cpp \
+           settingsdialog.cpp
+
+FORMS   += settingsdialog.ui
+
+unix {
+    target.path = $$PLUGIN_DIR/Input
+    INSTALLS += target
+    PKGCONFIG += libopenmpt
+    QMAKE_CLEAN = $$PLUGINS_PREFIX/Input/libopenmpt.so
+}
+
+win32 {
+    LIBS += -lopenmpt
+}
