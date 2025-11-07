430c2253f6c006ccd91d0093862df8c9df068658
Greedysky <greedysky@163.com>
2025-11-07 15:21:04 +0800

    feat: Support for qmmp 0.x Qt4

--- decoderopenmptfactory.cpp
+++ decoderopenmptfactory.cpp
@@ -130,3 +130,8 @@ QString DecoderOpenMPTFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(openmpt, DecoderOpenMPTFactory)
+#endif
